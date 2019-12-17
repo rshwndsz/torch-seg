6da7be6f6d0976fbb66d11c6680300ab38bd6351
Russel Dsouza <30742639+rshwndsz@users.noreply.github.com>
2019-12-17 17:26:54 +0530

    WIP: Add testing loop

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 # Python STL
 import os
+# Data Science
+import numpy as np
+import matplotlib.pyplot as plt
 # Image processing
 import cv2
 # PyTorch
+import torch
 from torch.utils.data import Dataset, DataLoader
 # Data Augmentations
 from albumentations.augmentations import transforms as T
@@ -10,7 +14,12 @@ from albumentations.core.composition import Compose
 from albumentations.pytorch import ToTensorV2
 
 # Local
-from .data import DATA_FOLDER
+from torchseg.data import DATA_FOLDER
+from torchseg.model import model
+from torchseg.trainer import Trainer
+
+_DIRNAME = os.path.dirname(__file__)
+_CHECKPOINT_PATH = os.path.join(_DIRNAME, "torchseg", "checkpoints", "model-2.pth")
 
 
 class TestDataset(Dataset):
@@ -45,18 +54,24 @@ test_loader = DataLoader(TestDataset(DATA_FOLDER),
 
 # TODO: Write code for evaluation with weight-loading
 
-# model = model_trainer.net # get the model from model_trainer object
-# model.eval()
-# state = torch.load('checkpoints/model-2.pth', map_location=lambda storage, loc: storage)
-# model.load_state_dict(state["state_dict"])
-# encoded_pixels = []
-# for i, batch in enumerate(tqdm(testset)):
-#     preds = torch.sigmoid(model(batch.to(device)))
-#     preds = preds.detach().cpu().numpy().squeeze()
-#     preds = np.where(preds>0.5, 1, preds)
-#     preds = np.where(preds<=0.5, 0, preds)
-#     print(preds.shape)
-#     plt.imshow(preds, 'gray')
-#     plt.show()
-#     plt.imshow(batch.cpu().numpy().squeeze().transpose(1, 2, 0))
-#     plt.show()
+if __name__ == "__main__":
+    # it = iter(test_loader)
+    # plt.imshow(next(it)[2].permute([1, 2, 0]))
+    # plt.show()
+    model_trainer = Trainer(model)
+    model = model_trainer.net   # get the model from model_trainer object
+    device = torch.device("cuda")
+    model.eval()
+    state = torch.load(_CHECKPOINT_PATH)
+    model.load_state_dict(state["state_dict"])
+
+    for i, batch in enumerate(test_loader):
+        preds = torch.sigmoid(model(batch.to(device)))
+        preds = preds.detach().cpu().numpy().squeeze()
+        preds = np.where(preds > 0.5, 1, preds)
+        preds = np.where(preds <= 0.5, 0, preds)
+        print(preds.shape)
+        plt.imshow(preds, 'gray')
+        plt.show()
+        plt.imshow(batch.cpu().numpy().squeeze().transpose(1, 2, 0))
+        plt.show()
