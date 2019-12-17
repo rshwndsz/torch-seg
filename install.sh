653caeb5d6d8adb4d66a786e6917a234746f9a89
Russel Dsouza <30742639+rshwndsz@users.noreply.github.com>
2019-12-17 11:25:36 +0530

    Add one-line bash script to install dataset

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=12hvl-VWteJoJsR4vdM4-0TZyJEuZJR61' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=12hvl-VWteJoJsR4vdM4-0TZyJEuZJR61" -O dataset.zip && rm -rf /tmp/cookies.txt
