418abba43c328581a65074d8cfb09f8b91d07bcc
Russel Dsouza <30742639+rshwndsz@users.noreply.github.com>
2019-12-19 12:49:21 +0530

    Add logging + aji + dice2 + pq dq sq
    * Add logging
    * Automate metric string
    * Add tested functions for AJI, DICE2
    * Add untested function for pq, dq, sq

--- torchseg/metrics.py
+++ torchseg/metrics.py
@@ -1,5 +1,6 @@
 # Python STL
 from datetime import datetime
+import logging
 import time
 # Data Science
 import numpy as np
@@ -8,6 +9,8 @@ import torch
 
 # Local
 from torchseg import utils
+
+logger = logging.getLogger()
 
 # TODO: Generalize to multiclass segmentation
 # TODO: Add tests to test integrity
@@ -196,7 +199,7 @@ def accuracy_score(preds, targets):
     return float(acc_sum) / (valid_sum + 1e-10)    # <<< Change: Hardcoded smoothing
 
 
-def iou_score(preds, targets, num_classes):
+def iou_score(preds, targets):
     """Computes IoU or Jaccard index
 
     Parameters
@@ -219,6 +222,214 @@ def iou_score(preds, targets, num_classes):
     return score.item()
 
 
+def get_fast_dice_2(pred, true):
+    """
+        Ensemble dice
+    """
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true_id = list(np.unique(true))
+    pred_id = list(np.unique(pred))
+
+    overall_total = 0
+    overall_inter = 0
+
+    true_masks = [np.zeros(true.shape)]
+    for t in true_id[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [np.zeros(true.shape)]
+    for p in pred_id[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    for true_idx in range(1, len(true_id)):
+        t_mask = true_masks[true_idx]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        try:  # blinly remove background
+            pred_true_overlap_id.remove(0)
+        except ValueError:
+            pass  # just mean no background
+        for pred_idx in pred_true_overlap_id:
+            p_mask = pred_masks[pred_idx]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            overall_total += total
+            overall_inter += inter
+
+    return 2 * overall_inter / overall_total
+
+
+def get_fast_pq(pred, true, match_iou=0.5):
+    """
+    `match_iou` is the IoU threshold level to determine the pairing between
+    GT instances `p` and prediction instances `g`. `p` and `g` is a pair
+    if IoU > `match_iou`. However, pair of `p` and `g` must be unique
+    (1 prediction instance to 1 GT instance mapping).
+    If `match_iou` < 0.5, Munkres assignment (solving minimum weight matching
+    in bipartite graphs) is caculated to find the maximal amount of unique pairing.
+    If `match_iou` >= 0.5, all IoU(p,g) > 0.5 pairing is proven to be unique and
+    the number of pairs is also maximal.
+
+    Fast computation requires instance IDs are in contiguous orderding
+    i.e [1, 2, 3, 4] not [2, 3, 6, 10]. Please call `remap_label` beforehand
+    and `by_size` flag has no effect on the result.
+    Returns:
+        [dq, sq, pq]: measurement statistic
+        [paired_true, paired_pred, unpaired_true, unpaired_pred]:
+                      pairing information to perform measurement
+
+    """
+    from scipy.optimize import linear_sum_assignment  # TODO: Move this import
+    assert match_iou >= 0.0, "Cant' be negative"
+
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true_id_list = list(np.unique(true))
+    pred_id_list = list(np.unique(pred))
+
+    true_masks = [None, ]
+    for t in true_id_list[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [None, ]
+    for p in pred_id_list[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    # prefill with value
+    pairwise_iou = np.zeros([len(true_id_list) - 1,
+                             len(pred_id_list) - 1], dtype=np.float64)
+
+    # caching pairwise iou
+    for true_id in true_id_list[1:]:  # 0-th is background
+        t_mask = true_masks[true_id]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        for pred_id in pred_true_overlap_id:
+            if pred_id == 0:  # ignore
+                continue  # overlaping background
+            p_mask = pred_masks[pred_id]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            iou = inter / (total - inter)
+            pairwise_iou[true_id - 1, pred_id - 1] = iou
+    #
+    if match_iou >= 0.5:
+        paired_iou = pairwise_iou[pairwise_iou > match_iou]
+        pairwise_iou[pairwise_iou <= match_iou] = 0.0
+        paired_true, paired_pred = np.nonzero(pairwise_iou)
+        paired_iou = pairwise_iou[paired_true, paired_pred]
+        paired_true += 1  # index is instance id - 1
+        paired_pred += 1  # hence return back to original
+    else:  # * Exhaustive maximal unique pairing
+        # <<< Munkres pairing with scipy library
+        # the algorithm return (row indices, matched column indices)
+        # if there is multiple same cost in a row, index of first occurence
+        # is return, thus the unique pairing is ensure
+        # inverse pair to get high IoU as minimum
+        paired_true, paired_pred = linear_sum_assignment(-pairwise_iou)
+        # <<< extract the paired cost and remove invalid pair
+        paired_iou = pairwise_iou[paired_true, paired_pred]
+
+        # now select those above threshold level
+        # paired with iou = 0.0 i.e no intersection => FP or FN
+        paired_true = list(paired_true[paired_iou > match_iou] + 1)
+        paired_pred = list(paired_pred[paired_iou > match_iou] + 1)
+        paired_iou = paired_iou[paired_iou > match_iou]
+
+    # get the actual FP and FN
+    unpaired_true = [idx for idx in true_id_list[1:] if idx not in paired_true]
+    unpaired_pred = [idx for idx in pred_id_list[1:] if idx not in paired_pred]
+    # print(paired_iou.shape, paired_true.shape, len(unpaired_true), len(unpaired_pred))
+
+    #
+    tp = len(paired_true)
+    fp = len(unpaired_pred)
+    fn = len(unpaired_true)
+    # get the F1-score i.e DQ
+    dq = tp / (tp + 0.5 * fp + 0.5 * fn)
+    # get the SQ, no paired has 0 iou so not impact
+    sq = paired_iou.sum() / (tp + 1.0e-6)
+
+    return [dq, sq, dq * sq], [paired_true, paired_pred, unpaired_true, unpaired_pred]
+
+
+def get_fast_aji(pred, true):
+    """
+    AJI version distributed by MoNuSeg, has no permutation problem but suffered from
+    over-penalisation similar to DICE2
+    Fast computation requires instance IDs are in contiguous orderding i.e [1, 2, 3, 4]
+    not [2, 3, 6, 10]. Please call `remap_label` before hand and `by_size` flag has no
+    effect on the result.
+    """
+    true = np.copy(true)  # ? do we need this
+    pred = np.copy(pred)
+    true_id_list = list(np.unique(true))
+    pred_id_list = list(np.unique(pred))
+
+    true_masks = [None, ]
+    for t in true_id_list[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [None, ]
+    for p in pred_id_list[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    # prefill with value
+    pairwise_inter = np.zeros([len(true_id_list) - 1,
+                               len(pred_id_list) - 1], dtype=np.float64)
+    pairwise_union = np.zeros([len(true_id_list) - 1,
+                               len(pred_id_list) - 1], dtype=np.float64)
+
+    # caching pairwise
+    for true_id in true_id_list[1:]:  # 0-th is background
+        t_mask = true_masks[true_id]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        for pred_id in pred_true_overlap_id:
+            if pred_id == 0:  # ignore
+                continue  # overlaping background
+            p_mask = pred_masks[pred_id]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            pairwise_inter[true_id - 1, pred_id - 1] = inter
+            pairwise_union[true_id - 1, pred_id - 1] = total - inter
+    #
+    pairwise_iou = pairwise_inter / (pairwise_union + 1.0e-6)
+    # pair of pred that give highest iou for each true, dont care
+    # about reusing pred instance multiple times
+    paired_pred = np.argmax(pairwise_iou, axis=1)
+    pairwise_iou = np.max(pairwise_iou, axis=1)
+    # exlude those dont have intersection
+    paired_true = np.nonzero(pairwise_iou > 0.0)[0]
+    paired_pred = paired_pred[paired_true]
+    # print(paired_true.shape, paired_pred.shape)
+    overall_inter = (pairwise_inter[paired_true, paired_pred]).sum()
+    overall_union = (pairwise_union[paired_true, paired_pred]).sum()
+    #
+    paired_true = (list(paired_true + 1))  # index to instance ID
+    paired_pred = (list(paired_pred + 1))
+    # add all unpaired GT and Prediction into the union
+    unpaired_true = np.array([idx for idx in true_id_list[1:] if idx not in paired_true])
+    unpaired_pred = np.array([idx for idx in pred_id_list[1:] if idx not in paired_pred])
+    for true_id in unpaired_true:
+        overall_union += true_masks[true_id].sum()
+    for pred_id in unpaired_pred:
+        overall_union += pred_masks[pred_id].sum()
+    #
+    aji_score = overall_inter / overall_union
+    return aji_score
+
+
 class Meter:
     """A meter to keep track of losses and scores"""
 
@@ -228,7 +439,10 @@ class Meter:
             'dice': [],
             'iou': [],
             'acc': [],
-            'prec': []
+            'prec': [],
+            'pq': [],
+            'aji': [],
+            'dice_2': []
         }
 
     def update(self, targets, logits):
@@ -251,18 +465,27 @@ class Meter:
             raise ValueError("Shape of preds: {} must be the same as that of targets: {}."
                              .format(preds.shape, targets.shape))
 
-        # TODO: Automate
+        # Calculate and add to metric lists
         dice = dice_score(probs, targets, self.base_threshold)
         self.metrics['dice'].append(dice)
 
-        iou = iou_score(preds, targets, num_classes=2)  # <<< TODO: Remove hardcoded num_classes
+        iou = iou_score(preds, targets)
         self.metrics['iou'].append(iou)
 
         acc = accuracy_score(preds, targets)
         self.metrics['acc'].append(acc)
 
-        prec = precision_score(preds, targets, num_classes=2)
+        prec = precision_score(preds, targets, num_classes=2)  # <<< TODO: Remove hardcoded num_classes
         self.metrics['prec'].append(prec)
+
+        pq = get_fast_pq(preds.long(), targets.long())
+        self.metrics['pq'].append(pq)
+
+        aji = get_fast_aji(preds.long(), targets.long())
+        self.metrics['aji'].append(aji)
+
+        dice_2 = get_fast_dice_2(preds.long(), targets.long())
+        self.metrics['dice_2'].append(dice_2)
 
     def get_metrics(self):
         """Compute mean of batchwise metrics
@@ -304,9 +527,11 @@ class Meter:
         end_time = time.strftime(fmt, time.localtime())
         delta_t = (datetime.strptime(end_time, fmt) - datetime.strptime(start_time, fmt))
 
-        # TODO: Automate
-        print(f"Loss: {epoch_loss:.4f} | dice: {metrics['dice']:.4f} | "
-              f"IoU: {metrics['iou']:4f} | Acc: {metrics['acc']:4f} | "
-              f"Prec: {metrics['prec']:4f} in {delta_t}")
+        # Construct string for logging
+        metric_string = f"Loss: {epoch_loss:.4f} | "
+        for metric_name, metric_value in metrics.items():
+            metric_string += f"{metric_name}: {metric_value:.4f} | "
+        metric_string += f"in {delta_t}"
 
+        logger.info(f"{metric_string}")
         return metrics
