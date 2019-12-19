2f37ff54a6735ec7faf349ea3d55fb26e6bf8d0c
Russel Dsouza <30742639+rshwndsz@users.noreply.github.com>
2019-12-19 12:46:39 +0530

    Add logging + move get_transforms()
    * Add logging to data.py
    * Move get_transforms() inside Dataset as staticmethod

--- torchseg/data.py
+++ torchseg/data.py
@@ -1,5 +1,6 @@
 # Python STL
 import os
+import logging
 # Image Processing
 import cv2
 # PyTorch
@@ -13,6 +14,8 @@ from albumentations.pytorch import ToTensorV2
 # Root folder of dataset
 dirname = os.path.dirname(__file__)
 DATA_FOLDER = os.path.join(dirname, "dataset/raw/")
+
+logger = logging.getLogger()
 
 
 # TODO: Generalize binary segmentation to multiclass segmentation
@@ -32,34 +35,39 @@ class OrganDataset(Dataset):
         class_dict : dict[int, int]
             Dictionary mapping brightness to class indices
         """
+        logger.info(f"Creating {phase} dataset ... ")
+
         # Root folder of the dataset
         assert os.path.isdir(data_folder), "{} is not a directory or it doesn't exist.".format(data_folder)
+        logger.info(f"Datafolder: {data_folder}")
         self.root = data_folder
 
         # Phase of learning
-        assert phase in ['train', 'test', 'val'], "Provide any one of train/test/val as phase."
+        assert phase in ['train', 'val'], "Provide any one of ['train', 'val'] as phase."
+        logger.info(f"Phase: {phase}")
         self.phase = phase
 
         # Data Augmentations and tensor transformations
-        self.transforms = get_transforms(self.phase)
+        self.transforms = OrganDataset.get_transforms(self.phase)
 
         # Get names & number of images in root/train or root/val
         _path_to_imgs = os.path.join(self.root, self.phase, "imgs")
-        assert os.path.isdir(_path_to_imgs), "{} doesn't exist.".format(_path_to_imgs)
+        assert os.path.isdir(_path_to_imgs), f"{_path_to_imgs} doesn't exist."
         self.image_names = sorted(os.listdir(_path_to_imgs))
-        assert len(self.image_names) != 0, "No images found in {}".format(_path_to_imgs)
+        assert len(self.image_names) != 0, f"No images found in {_path_to_imgs}"
+        logger.info(f"Found {len(self.image_names)} images in {_path_to_imgs}")
 
         # Number of classes in the segmentation target
-        assert num_classes >= 2, "Number of classes must be >= 2. 2: Binary, >=2: Multi-class"
+        assert num_classes >= 2, "Number of classes must be >= 2. 2: Binary, >2: Multi-class"
         assert isinstance(num_classes, int), "Number of classes must be an integer."
         self.num_classes = num_classes
 
         # Dictionary specifying the mapping between pixel values [0, 255] and class indices [0, C-1]
         assert len(class_dict) == num_classes, "Length of class dict must be same as number of classes."
-        assert max(class_dict) == 255, "Max intensity of grayscale images is 255, " \
-                                       "but class dict: {} specifies otherwise".format(class_dict)
-        assert min(class_dict) == 0, "Min intensity of grayscale images is 0, " \
-                                     "but class dict: {} specifies otherwise".format(class_dict)
+        assert max(class_dict) == 255, f"Max intensity of grayscale images is 255, " \
+                                       f"but class dict: {class_dict} specifies otherwise"
+        assert min(class_dict) == 0, f"Min intensity of grayscale images is 0, " \
+                                     f"but class dict: {class_dict} specifies otherwise"
         self.class_dict = class_dict
 
     def __getitem__(self, idx):
@@ -92,73 +100,69 @@ class OrganDataset(Dataset):
     def __len__(self):
         return len(self.image_names)
 
+    @staticmethod
+    def get_transforms(phase):
+        """Get composed albumentations augmentations
 
-# TODO: Make it easier to add augmentations
-# TODO: Add logging here
-# TODO: Move into DataSet as static method
-def get_transforms(phase):
-    """Get composed albumentations augmentations
+        Parameters
+        ----------
+        phase : str
+            Phase of learning
+            In ['train', 'val']
 
-    Parameters
-    ----------
-    phase : str
-        Phase of learning
-        In ['train', 'val']
+        Returns
+        -------
+        transforms: dict[str, albumentations.core.composition.Compose]
+            Composed list of transforms
+        """
+        aug_transforms = []
 
-    Returns
-    -------
-    transforms: dict[str, albumentations.core.composition.Compose]
-        Composed list of transforms
-    """
-    aug_transforms = []
-
-    if phase == "train":
-        # Data augmentation for training only
+        if phase == "train":
+            # Data augmentation for training only
+            aug_transforms.extend([
+                T.ShiftScaleRotate(
+                    shift_limit=0,
+                    scale_limit=0.1,
+                    rotate_limit=15,
+                    p=0.5),
+                T.Flip(p=0.5),
+                T.RandomRotate90(p=0.5),
+            ])
+            # Exotic Augmentations for train only 🤤
+            aug_transforms.extend([
+                T.RandomBrightnessContrast(p=0.5),
+                T.ElasticTransform(p=0.5),
+                T.MultiplicativeNoise(multiplier=(0.5, 1.5), per_channel=True, p=0.2),
+            ])
         aug_transforms.extend([
-            T.ShiftScaleRotate(
-                shift_limit=0,
-                scale_limit=0.1,
-                rotate_limit=15,
-                p=0.5),
-            T.Flip(p=0.5),
-            T.RandomRotate90(p=0.5),
+            T.RandomSizedCrop(min_max_height=(256, 256),
+                              height=256,
+                              width=256,
+                              w2h_ratio=1.0,
+                              interpolation=cv2.INTER_LINEAR,
+                              p=1.0),
         ])
-        # Exotic Augmentations for train only 🤤
-        aug_transforms.extend([
-            T.RandomBrightnessContrast(p=0.5),
-            T.ElasticTransform(p=0.5),
-            T.MultiplicativeNoise(multiplier=(0.5, 1.5), per_channel=True, p=0.2),
+        aug_transforms = Compose(aug_transforms)
+
+        mask_only_transforms = Compose([
+            T.Normalize(mean=0, std=1, always_apply=True)
         ])
-    aug_transforms.extend([
-        T.RandomSizedCrop(min_max_height=(256, 256),
-                          height=256,
-                          width=256,
-                          w2h_ratio=1.0,
-                          interpolation=cv2.INTER_LINEAR,
-                          p=1.0),
-    ])
-    aug_transforms = Compose(aug_transforms)
+        image_only_transforms = Compose([
+            T.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0), always_apply=True)
+        ])
+        final_transforms = Compose([
+            ToTensorV2()
+        ])
 
-    mask_only_transforms = Compose([
-        T.Normalize(mean=0, std=1, always_apply=True)
-    ])
-    image_only_transforms = Compose([
-        T.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0), always_apply=True)
-    ])
-    final_transforms = Compose([
-        ToTensorV2()
-    ])
-
-    transforms = {
-        'aug': aug_transforms,
-        'img_only': image_only_transforms,
-        'mask_only': mask_only_transforms,
-        'final': final_transforms
-    }
-    return transforms
+        transforms = {
+            'aug': aug_transforms,
+            'img_only': image_only_transforms,
+            'mask_only': mask_only_transforms,
+            'final': final_transforms
+        }
+        return transforms
 
 
-# TODO: Add logging here
 def provider(data_folder, phase, batch_size=8, num_workers=4):
     """Return dataloader for the dataset
 
@@ -180,6 +184,7 @@ def provider(data_folder, phase, batch_size=8, num_workers=4):
         DataLoader for loading data from CPU to GPU
     """
     image_dataset = OrganDataset(data_folder, phase)
+    logging.info(f"Creating {phase} dataloader ... ")
     dataloader = DataLoader(
         image_dataset,
         batch_size=batch_size,
